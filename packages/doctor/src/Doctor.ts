62413632807d36112d25a1d5edc0accc3c8c4d2f
Dor Schrieber <schreiberdor@gmail.com>
2022-09-27 14:49:45 +0300

    init

---
+++ packages/doctor/src/Doctor.ts
@@ -0,0 +1,3 @@
+export class Doctor{
+private ss:number;
+};
---
+++ packages/integration/src/index.ts
@@ -0,0 +1,7 @@
+import { Doctor } from "@monorepo-example/doctor";
+import { Patient } from "@monorepo-example/patient";
+import { Scheduler } from "@monorepo-example/scheduler";
+
+console.log(new Doctor());
+console.log(new Scheduler());
+console.log(new Patient());
